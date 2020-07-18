7c1faa10e0d459d237498ded43520b798dfee41b
Anand Shankar <theoptimisticcoder@gmail.com>
2020-07-18 23:57:50 +0530

    added error messages

---
+++ utils/errorMessages.js
@@ -0,0 +1,8 @@
+const errorMessages = {
+    "name": "Name must be 3-30 characters long",
+    "email": "Please enter a valid email",
+    "username": "Min Length - 3, Max Length - 30, Characters - [a-z,A-Z,0-9,_]",
+    "password": "Password must be 8-30 characters long"
+}
+
+module.exports = errorMessages
