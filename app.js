a0d1958577ae1e9a8118d124e752f5bc48e8ead1
Anand Shankar <theoptimisticcoder@gmail.com>
2020-07-27 10:58:03 +0530

    app modified

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
+
+const uri = process.env.DB_URI
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+mongoose.connection.once('open', () => console.log('Connected to DB'))
+
+const users = require('./routes/users')
+
+app.use('/users', users)
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+})
